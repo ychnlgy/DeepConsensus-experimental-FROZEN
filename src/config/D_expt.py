fa38d716b35a2dd574ff9e6b87ac9a2f372679d2
Yuchen Li <ychnlgy@gmail.com>
2018-10-07 16:58:40 -0400

    Testing avgpool

--- src/config/D_expt.py
+++ src/config/D_expt.py
@@ -18,11 +18,11 @@ class D_expt(Base):
             torch.nn.LeakyReLU(),
             torch.nn.BatchNorm2d(128),
             
-#            # 28 -> 14
-#            torch.nn.Conv2d(64, 32, 3, padding=1, stride=1),
-#            torch.nn.AvgPool2d(2, padding=0, stride=2),
-#            torch.nn.LeakyReLU(),
-#            torch.nn.BatchNorm2d(32),
+            # 28 -> 14
+            torch.nn.Conv2d(128, 32, 3, padding=1, stride=1),
+            torch.nn.AvgPool2d(2, padding=0, stride=2),
+            torch.nn.LeakyReLU(),
+            torch.nn.BatchNorm2d(32),
             
 #            # 14 -> 14
 #            torch.nn.Conv2d(32, 32, 3, padding=1, stride=1),
@@ -57,16 +57,16 @@ class D_expt(Base):
             # 14 -> 7
             models.DistillationLayer(
                 interpreter = models.DenseNet(
-                    headsize = 128,
-                    bodysize = 64,
-                    tailsize = 32,
+                    headsize = 32,
+                    bodysize = 128,
+                    tailsize = 64,
                     layers = 2,
                     dropout = 0.2,
                     bias = True
                 ),
                 summarizer = models.DenseNet(
-                    headsize = 32,
-                    bodysize = 64,
+                    headsize = 64,
+                    bodysize = 128,
                     tailsize = 16,
                     layers = 2,
                     dropout = 0.2,
@@ -76,6 +76,8 @@ class D_expt(Base):
                 stride = 2,
                 padding = 1
             ),
+            
+            torch.nn.BatchNorm2d(16),
             
             # 7 -> 4
             models.DistillationLayer(
