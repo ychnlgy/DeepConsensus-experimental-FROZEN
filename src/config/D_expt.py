bdb8717e9c886ed6307eb333b23838b80bda72b7
Yuchen Li <ychnlgy@gmail.com>
2018-10-07 17:47:53 -0400

    3 removed all dropout

--- src/config/D_expt.py
+++ src/config/D_expt.py
@@ -13,63 +13,34 @@ class D_expt(Base):
     def create_net(self, classes, channels):
         return torch.nn.Sequential( # Parameter count:
             
-            # 28 -> 28
-            torch.nn.Conv2d(channels, 64, 3, padding=2, stride=1),
+            # 28 -> 14
+            torch.nn.Conv2d(channels, 128, 3, padding=1, stride=1),
+            torch.nn.AvgPool2d(2),
+            torch.nn.LeakyReLU(),
+            torch.nn.BatchNorm2d(128),
+            
+            # 14 -> 7
+            torch.nn.Conv2d(128, 64, 3, padding=1, stride=1),
+            torch.nn.AvgPool2d(2),
             torch.nn.LeakyReLU(),
             torch.nn.BatchNorm2d(64),
             
-            # 28 -> 14
-            torch.nn.Conv2d(64, 32, 3, padding=1, stride=1),
-            torch.nn.AvgPool2d(2, padding=0, stride=2),
-            torch.nn.LeakyReLU(),
-            torch.nn.BatchNorm2d(32),
-            
-            # 14 -> 14
-            torch.nn.Conv2d(32, 32, 3, padding=1, stride=1),
-            torch.nn.LeakyReLU(),
-            torch.nn.BatchNorm2d(32),
-            
-#            # 14 -> 14
-#            models.DistillationLayer(
-#                interpreter = models.DenseNet(
-#                    headsize = 32,
-#                    bodysize = 128,
-#                    tailsize = 64,
-#                    layers = 2,
-#                    dropout = 0.2,
-#                    bias = True
-#                ),
-#                summarizer = models.DenseNet(
-#                    headsize = 64,
-#                    bodysize = 256,
-#                    tailsize = 32,
-#                    layers = 2,
-#                    dropout = 0.2,
-#                    bias = True
-#                ),
-#                kernel = 3,
-#                stride = 1,
-#                padding = 1
-#            ),
-#            
-#            torch.nn.BatchNorm2d(32),
-            
-            # 14 -> 7
+            # 7 -> 4
             models.DistillationLayer(
                 interpreter = models.DenseNet(
-                    headsize = 32,
-                    bodysize = 128,
-                    tailsize = 64,
+                    headsize = 64,
+                    bodysize = 256,
+                    tailsize = 128,
                     layers = 2,
-                    dropout = 0.1,
+                    dropout = 0.0,
                     bias = True
                 ),
                 summarizer = models.DenseNet(
-                    headsize = 64,
-                    bodysize = 128,
+                    headsize = 128,
+                    bodysize = 256,
                     tailsize = 32,
                     layers = 2,
-                    dropout = 0.1,
+                    dropout = 0.0,
                     bias = True
                 ),
                 kernel = 3,
@@ -79,14 +50,14 @@ class D_expt(Base):
             
             torch.nn.BatchNorm2d(32),
             
-            # 7 -> 4
+            # 4 -> 2
             models.DistillationLayer(
                 interpreter = models.DenseNet(
                     headsize = 32,
                     bodysize = 128,
                     tailsize = 64,
                     layers = 2,
-                    dropout = 0.1,
+                    dropout = 0.0,
                     bias = True
                 ),
                 summarizer = models.DenseNet(
@@ -94,7 +65,7 @@ class D_expt(Base):
                     bodysize = 128,
                     tailsize = 16,
                     layers = 2,
-                    dropout = 0.1,
+                    dropout = 0.0,
                     bias = True
                 ),
                 kernel = 3,
@@ -104,7 +75,7 @@ class D_expt(Base):
             
             torch.nn.BatchNorm2d(16),
             
-            models.Reshape(len, 16, 16, contiguous=True),
+            models.Reshape(len, 16, 4, contiguous=True),
             models.Permute(0, 2, 1), # N, W*H, C
             models.Mean(dim=1), # N, C
             
@@ -113,7 +84,7 @@ class D_expt(Base):
                 bodysize = 32,
                 tailsize = classes,
                 layers = 2,
-                dropout = 0.05,
+                dropout = 0.0,
                 bias = True
             ),
         )
