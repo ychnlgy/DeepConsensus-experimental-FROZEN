46235716d68f7170f22b41deb1924f514e67ea48
Yuchen Li <ychnlgy@gmail.com>
2018-10-07 16:46:21 -0400

    Added experimental distillation network

--- src/config/D_expt.py
+++ src/config/D_expt.py
@@ -24,18 +24,23 @@ class D_expt(Base):
             torch.nn.LeakyReLU(),
             torch.nn.BatchNorm2d(32),
             
+            # 14 -> 14
+            torch.nn.Conv2d(32, 32, 3, padding=1, stride=1),
+            torch.nn.LeakyReLU(),
+            torch.nn.BatchNorm2d(32),
+            
             # 14 -> 7
             models.DistillationLayer(
                 interpreter = models.DenseNet(
                     headsize = 32,
                     bodysize = 128,
-                    tailsize = 128,
+                    tailsize = 64,
                     layers = 2,
                     dropout = 0.2,
                     bias = True
                 ),
                 summarizer = models.DenseNet(
-                    headsize = 128,
+                    headsize = 64,
                     bodysize = 128,
                     tailsize = 16,
                     layers = 2,
@@ -54,13 +59,13 @@ class D_expt(Base):
                 interpreter = models.DenseNet(
                     headsize = 16,
                     bodysize = 64,
-                    tailsize = 64,
+                    tailsize = 32,
                     layers = 2,
                     dropout = 0.2,
                     bias = True
                 ),
                 summarizer = models.DenseNet(
-                    headsize = 64,
+                    headsize = 32,
                     bodysize = 64,
                     tailsize = 16,
                     layers = 2,
