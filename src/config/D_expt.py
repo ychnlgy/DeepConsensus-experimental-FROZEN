aa4dea2fb9e034287e81efeca5e46e9839c1ae2c
Yuchen Li <ychnlgy@gmail.com>
2018-10-08 17:36:17 -0400

    Trying extremely simple distillation layer

--- src/config/D_expt.py
+++ src/config/D_expt.py
@@ -16,19 +16,19 @@ class D_expt(Base):
             # 28 -> 28
             models.DistillationLayer(
                 cnn = torch.nn.Sequential(
-                    torch.nn.Conv2d(channels, 32, 3, padding=1),
-                    torch.nn.BatchNorm2d(32),
+                    torch.nn.Conv2d(channels, 128, 3, padding=1),
+                    torch.nn.BatchNorm2d(128),
                 ),
-                lin = torch.nn.Linear(32, 32)
+                lin = torch.nn.Linear(128, 64)
             ),
             
             # 28 -> 14
             models.DistillationLayer(
                 cnn = torch.nn.Sequential(
-                    torch.nn.Conv2d(32, 32, 3, padding=1, stride=2, groups=32),
-                    torch.nn.BatchNorm2d(32),
+                    torch.nn.Conv2d(64, 64, 3, padding=1, stride=2, groups=32),
+                    torch.nn.BatchNorm2d(64),
                 ),
-                lin = torch.nn.Linear(32, 32)
+                lin = torch.nn.Linear(64, 32)
             ),
             
             # 14 -> 7
