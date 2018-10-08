5b9f43c462306c5953a4eb4d9388989429a29f4b
Yuchen Li <ychnlgy@gmail.com>
2018-10-07 21:09:44 -0400

    One layer interpreters

--- src/config/D_expt.py
+++ src/config/D_expt.py
@@ -19,30 +19,30 @@ class D_expt(Base):
             torch.nn.BatchNorm2d(256),
             
             # 28 -> 14
-            torch.nn.Conv2d(256, 128, 3, padding=1, stride=1),
-            torch.nn.MaxPool2d(2),
+            torch.nn.Conv2d(256, 64, 3, padding=1, stride=1),
+            torch.nn.AvgPool2d(2),
             torch.nn.LeakyReLU(),
-            torch.nn.BatchNorm2d(128),
+            torch.nn.BatchNorm2d(64),
             
             # 14 -> 14
-            torch.nn.Conv2d(128, 128, 3, padding=1, stride=1),
+            torch.nn.Conv2d(64, 64, 3, padding=1, stride=1),
             torch.nn.LeakyReLU(),
-            torch.nn.BatchNorm2d(128),
+            torch.nn.BatchNorm2d(64),
             
             # 14 -> 7
             models.DistillationLayer(
                 interpreter = models.DenseNet(
-                    headsize = 128,
-                    bodysize = 256,
-                    tailsize = 256,
+                    headsize = 64,
+                    bodysize = 128,
+                    tailsize = 128,
                     layers = 1,
                     dropout = 0.2,
                     bias = True
                 ),
                 summarizer = models.DenseNet(
-                    headsize = 256,
-                    bodysize = 128,
-                    tailsize = 64,
+                    headsize = 128,
+                    bodysize = 64,
+                    tailsize = 32,
                     layers = 2,
                     dropout = 0.2,
                     bias = True
@@ -52,7 +52,7 @@ class D_expt(Base):
                 padding = 1
             ),
             
-            torch.nn.BatchNorm2d(64),
+            torch.nn.BatchNorm2d(32),
             
             # 7 -> 4
             models.DistillationLayer(
