8d280279fc7387e8790880589f1ff7e446394b40
Yuchen Li <ychnlgy@gmail.com>
2018-10-07 22:48:22 -0400

    Testing PreLU

--- src/config/D_expt.py
+++ src/config/D_expt.py
@@ -25,24 +25,24 @@ class D_expt(Base):
             torch.nn.BatchNorm2d(64),
             
             # 14 -> 14
-            torch.nn.Conv2d(64, 32, 3, padding=1, stride=1),
+            torch.nn.Conv2d(64, 64, 3, padding=1, stride=1),
             torch.nn.PReLU(),
-            torch.nn.BatchNorm2d(32),
+            torch.nn.BatchNorm2d(64),
             
             # 14 -> 7
             models.DistillationLayer(
                 interpreter = models.DenseNet(
-                    headsize = 32,
-                    bodysize = 64,
-                    tailsize = 64,
+                    headsize = 64,
+                    bodysize = 128,
+                    tailsize = 128,
                     layers = 1,
                     dropout = 0.2,
                     bias = False
                 ),
                 summarizer = models.DenseNet(
-                    headsize = 64,
-                    bodysize = 32,
-                    tailsize = 16,
+                    headsize = 128,
+                    bodysize = 64,
+                    tailsize = 32,
                     layers = 2,
                     dropout = 0.2,
                     bias = False
@@ -52,22 +52,22 @@ class D_expt(Base):
                 padding = 1
             ),
             
-            torch.nn.BatchNorm2d(16),
+            torch.nn.BatchNorm2d(32),
             
             # 7 -> 4
             models.DistillationLayer(
                 interpreter = models.DenseNet(
-                    headsize = 16,
-                    bodysize = 32,
-                    tailsize = 32,
+                    headsize = 32,
+                    bodysize = 64,
+                    tailsize = 64,
                     layers = 1,
                     dropout = 0.0,
                     bias = False
                 ),
                 summarizer = models.DenseNet(
-                    headsize = 32,
-                    bodysize = 32,
-                    tailsize = 8,
+                    headsize = 64,
+                    bodysize = 64,
+                    tailsize = 16,
                     layers = 1,
                     dropout = 0.1,
                     bias = False
@@ -77,12 +77,12 @@ class D_expt(Base):
                 padding = 1
             ),
             
-            torch.nn.BatchNorm2d(8),
+            torch.nn.BatchNorm2d(16),
             
             # 4 -> 1
             models.DistillationLayer(
                 interpreter = models.DenseNet(
-                    headsize = 8,
+                    headsize = 16,
                     bodysize = 32,
                     tailsize = 32,
                     layers = 1,
