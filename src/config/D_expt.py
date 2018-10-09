38d5f54754c166c51c77c5ff69524344e22d9151
Yuchen Li <ychnlgy@gmail.com>
2018-10-09 00:05:56 -0400

    Adding smaller network

--- src/config/D_expt.py
+++ src/config/D_expt.py
@@ -12,7 +12,7 @@ class D_expt(Base):
 
     def create_net(self, classes, channels):
     
-        initial_channels = 96 * 3
+        initial_channels = 64 * 3
     
         return torch.nn.Sequential( # Parameter count: 98274
             
@@ -62,52 +62,52 @@ class D_expt(Base):
                 summarizer = models.DenseNet(
                     headsize = 64,
                     bodysize = 64,
-                    tailsize = 16,
+                    tailsize = 32,
                     layers = 1,
                     dropout = 0.1,
                     bias = True
                 )
             ),
             
-            torch.nn.BatchNorm2d(16),
+            torch.nn.BatchNorm2d(32),
             
             # 7 -> 4
             models.DistillationLayer(
                 interpreter = models.DenseNet(
-                    headsize = 16,
-                    bodysize = 32,
-                    tailsize = 32,
+                    headsize = 32,
+                    bodysize = 64,
+                    tailsize = 64,
                     layers = 1,
                     dropout = 0.1,
                     bias = True
                 ),
                 pool = torch.nn.AvgPool2d(3, padding=1, stride=2),
                 summarizer = models.DenseNet(
-                    headsize = 32,
-                    bodysize = 32,
-                    tailsize = 16,
+                    headsize = 64,
+                    bodysize = 64,
+                    tailsize = 32,
                     layers = 1,
                     dropout = 0.1,
                     bias = True
                 )
             ),
             
-            torch.nn.BatchNorm2d(16),
+            torch.nn.BatchNorm2d(32),
             
             # 4 -> 1
             models.DistillationLayer(
                 interpreter = models.DenseNet(
-                    headsize = 16,
-                    bodysize = 32,
-                    tailsize = 32,
+                    headsize = 32,
+                    bodysize = 64,
+                    tailsize = 64,
                     layers = 1,
                     dropout = 0.1,
                     bias = True
                 ),
                 pool = torch.nn.AvgPool2d(4),
                 summarizer = models.DenseNet(
-                    headsize = 32,
-                    bodysize = 32,
+                    headsize = 64,
+                    bodysize = 64,
                     tailsize = classes,
                     layers = 1,
                     dropout = 0.1,
