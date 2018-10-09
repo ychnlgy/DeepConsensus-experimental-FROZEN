722880cbc8effa1caf62dd5b26d934115bf347e3
Yuchen Li <ychnlgy@gmail.com>
2018-10-08 20:02:55 -0400

    Reverting to original distillations

--- src/models/AssertShape.py
+++ src/models/AssertShape.py
@@ -7,5 +7,5 @@ class AssertShape(torch.nn.Module):
     
     def forward(self, X):
         if X.shape[1:] != self.size:
-            raise AssertionError("Shape %s does not match the expected value of %s." % (tuple(X.shape[1:], self.size)))
+            raise AssertionError("Shape %s does not match the expected value of %s." % (tuple(X.shape[1:]), self.size)))
         return X
