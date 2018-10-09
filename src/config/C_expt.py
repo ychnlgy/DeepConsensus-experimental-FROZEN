7a53f03e3a8da6daae0c0eba6b68062a13c6ee25
Yuchen Li <ychnlgy@gmail.com>
2018-10-09 00:13:18 -0400

    Adding smaller network

--- src/config/C_expt.py
+++ src/config/C_expt.py
@@ -26,13 +26,13 @@ class C_expt(Base):
             ),
             
             # 28 -> 14
-            torch.nn.Conv2d(initial_channels, 64, 3, padding=1, groups=2),
+            torch.nn.Conv2d(initial_channels, 64, 3, padding=1, groups=4),
             torch.nn.LeakyReLU(),
             torch.nn.MaxPool2d(2),
             torch.nn.BatchNorm2d(64),
             
             # 14 -> 7
-            torch.nn.Conv2d(64, 32, 3, padding=1, groups=2),
+            torch.nn.Conv2d(64, 32, 3, padding=1, groups=4),
             torch.nn.LeakyReLU(),
             torch.nn.MaxPool2d(2),
             torch.nn.BatchNorm2d(32),
