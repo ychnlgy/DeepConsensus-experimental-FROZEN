80ebfbf723017ac233d179e143b5a3d18709910e
Yuchen Li <ychnlgy@gmail.com>
2018-10-12 18:27:58 -0400

    Debugging adversarial approach

--- src/models/Discriminator.py
+++ src/models/Discriminator.py
@@ -6,10 +6,17 @@ class Discriminator(Savable):
     
     def __init__(self, classifier, net):
         super(Discriminator, self).__init__()
+        self.classifier = classifier
         self.params = torch.nn.Parameter(
-            torch.cat([p.view(-1) for p in classifier.parameters()])
+            self.get_classifierparams()
         )
         self.net = net
     
     def forward(self):
+        assert (self.get_classifierparams() == self.params).all()
         return self.net(self.params)
+    
+    def get_classifierparams(self):
+        return torch.cat([
+            p.view(-1) for p in self.classifier.parameters()
+        ])
