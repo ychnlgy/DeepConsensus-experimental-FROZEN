329f0267f9c9e56973d3946094d4d48d175a97df
Yuchen Li <ychnlgy@gmail.com>
2018-10-13 15:28:44 -0400

    Trying distillation on every layer

--- src/models/DistillLayer.py
+++ src/models/DistillLayer.py
@@ -9,8 +9,8 @@ class DistillLayer(torch.nn.Module):
         self.summarizer = summarizer
     
     def forward(self, X):
-        N, C, W, H = X.size()
         convout = self.convlayer(X)
+        N, C, W, H = convout.size()
         infovec = convout.permute(0, 2, 3, 1).view(N, W*H, C)
         counted = self.counter(infovec).mean(dim=1)
         summary = self.summarizer(counted)
