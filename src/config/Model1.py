906ca46faeadf51233d445ee72668689f8ba513f
Yuchen Li <ychnlgy@gmail.com>
2018-10-13 19:42:59 -0400

    MAtching Conv with only average

--- src/config/Model1.py
+++ src/config/Model1.py
@@ -20,77 +20,53 @@ class Model(Base):
             # 28 -> 14
             
             # interpreter
-            torch.nn.Conv2d(128, 256, 3, padding=1, groups=128),
+            torch.nn.Conv2d(128, 256, 3, padding=1),
             torch.nn.LeakyReLU(),
             torch.nn.BatchNorm2d(256),
             
-            torch.nn.Sequential(
-                torch.nn.Conv2d(256, 256, 3, padding=1, groups=256),
-                torch.nn.AvgPool2d(2),
-                torch.nn.LeakyReLU(),
-                torch.nn.BatchNorm2d(256),
-                torch.nn.Conv2d(256, 128, 3, padding=1, groups=128),
-                torch.nn.LeakyReLU()
-            ),
+            torch.nn.AvgPool2d(2),
             
             # summarizer
-            torch.nn.Conv2d(128, 64, 3, padding=1, groups=64),
+            torch.nn.Conv2d(256, 64, 3, padding=1),
             torch.nn.LeakyReLU(),
             torch.nn.BatchNorm2d(64),
             
             # 14 -> 7
             
             # interpreter
-            torch.nn.Conv2d(64, 128, 3, padding=1, groups=64),
+            torch.nn.Conv2d(64, 128, 3, padding=1),
             torch.nn.LeakyReLU(),
             torch.nn.BatchNorm2d(128),
             
-            torch.nn.Sequential(
-                torch.nn.Conv2d(128, 128, 3, padding=1, groups=128),
-                torch.nn.AvgPool2d(2),
-                torch.nn.LeakyReLU(),
-                torch.nn.BatchNorm2d(128),
-                torch.nn.Conv2d(128, 64, 3, padding=1, groups=64),
-                torch.nn.LeakyReLU(),
-            ),
+            torch.nn.AvgPool2d(2),
             
             # summarizer
-            torch.nn.Conv2d(64, 32, 3, padding=1, groups=32),
+            torch.nn.Conv2d(128, 32, 3, padding=1),
             torch.nn.LeakyReLU(),
             torch.nn.BatchNorm2d(32),
             
             # 7 -> 4
             
             # interpreter
-            torch.nn.Conv2d(32, 64, 3, padding=1, groups=32),
+            torch.nn.Conv2d(32, 64, 3, padding=1),
             torch.nn.LeakyReLU(),
             torch.nn.BatchNorm2d(64),
             
-            torch.nn.Sequential(
-                torch.nn.Conv2d(64, 64, 3, padding=1, groups=64),
-                torch.nn.AvgPool2d(3, padding=1, stride=2),
-                torch.nn.LeakyReLU(),
-                torch.nn.Conv2d(64, 32, 3, padding=1, groups=32),
-                torch.nn.LeakyReLU(),
-            ),
+            torch.nn.AvgPool2d(3, padding=1, stride=2),
             
             # summarizer
-            torch.nn.Conv2d(32, 32, 3, padding=1, groups=32),
+            torch.nn.Conv2d(64, 32, 3, padding=1),
             torch.nn.LeakyReLU(),
             torch.nn.BatchNorm2d(32),
             
             # 4 -> 1
             
             # interpreter
-            torch.nn.Conv2d(32, 64, 3, padding=1, groups=32),
+            torch.nn.Conv2d(32, 64, 3, padding=1),
             torch.nn.LeakyReLU(),
             torch.nn.BatchNorm2d(64),
             
-            torch.nn.Sequential(
-                torch.nn.Conv2d(64, 64, 3, padding=1, groups=64),
-                torch.nn.AvgPool2d(4),
-                torch.nn.LeakyReLU()
-            ),
+            torch.nn.AvgPool2d(4),
             
             models.Reshape(64),
             models.DenseNet(
