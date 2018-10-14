a6734107b0a9c72df71ba3fb295c60bf620ed6af
Yuchen Li <ychnlgy@gmail.com>
2018-10-14 19:53:27 -0400

    Trying one layers

--- src/config/Model0.py
+++ src/config/Model0.py
@@ -24,43 +24,43 @@ class Model(Base):
                     interpreter = models.DenseNet(
                         headsize = 64,
                         bodysize = 128,
-                        tailsize = 64,
-                        layers = 2,
+                        tailsize = 128,
+                        layers = 1,
                         dropout = 0.2
                     ),
                     summarizer = models.DenseNet(
-                        headsize = 64,
+                        headsize = 128,
                         bodysize = 128,
                         tailsize = 32,
-                        layers = 2,
+                        layers = 1,
                         dropout = 0.2
                     ),
                 ),
             
-#                # 28 -> 14
-#                models.DistillLayer(
-#                    convlayer = torch.nn.Sequential(
-#                        torch.nn.Conv2d(64, 32, 3, padding=1, groups=32),
-#                        torch.nn.MaxPool2d(2),
-#                        torch.nn.LeakyReLU(),
-#                        torch.nn.BatchNorm2d(32),
-#                    ),
-#                    dropout = 0.2,
-#                    interpreter = models.DenseNet(
-#                        headsize = 32,
-#                        bodysize = 64,
-#                        tailsize = 32,
-#                        layers = 2,
-#                        dropout = 0.2
-#                    ),
-#                    summarizer = models.DenseNet(
-#                        headsize = 32,
-#                        bodysize = 64,
-#                        tailsize = 32,
-#                        layers = 2,
-#                        dropout = 0.2
-#                    ),
-#                ),
+                # 28 -> 14
+                models.DistillLayer(
+                    convlayer = torch.nn.Sequential(
+                        torch.nn.Conv2d(64, 64, 3, padding=1, groups=64),
+                        torch.nn.MaxPool2d(2),
+                        torch.nn.LeakyReLU(),
+                        torch.nn.BatchNorm2d(32),
+                    ),
+                    dropout = 0.2,
+                    interpreter = models.DenseNet(
+                        headsize = 64,
+                        bodysize = 64,
+                        tailsize = 64,
+                        layers = 1,
+                        dropout = 0.2
+                    ),
+                    summarizer = models.DenseNet(
+                        headsize = 64,
+                        bodysize = 64,
+                        tailsize = 32,
+                        layers = 1,
+                        dropout = 0.2
+                    ),
+                ),
                 
 #                # 14 -> 7
 #                models.DistillLayer(
@@ -140,8 +140,8 @@ class Model(Base):
             ),
             
             models.DenseNet(
-                headsize = 32,
-                bodysize = 64,
+                headsize = 32 + 32,
+                bodysize = 128,
                 tailsize = classes,
                 layers = 2,
                 dropout = 0.2
