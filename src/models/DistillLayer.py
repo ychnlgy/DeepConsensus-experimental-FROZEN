87214d027871c99a9209a322ea83ab6fd35537c3
Yuchen Li <ychnlgy@gmail.com>
2018-10-14 12:55:39 -0400

    Using interpreted channels as inputs to next convlayer

--- src/models/DistillLayer.py
+++ src/models/DistillLayer.py
@@ -16,6 +16,7 @@ class DistillLayer(torch.nn.Module):
         convinp = self.dropout(convout)
         interpd = self.interpreter(convinp.permute(0, 2, 3, 1)) # N, W, H, C
         N, W, H, C = interpd.size()
+        convout = interpd.permute(0, 3, 1, 2) # N, C, W, H
         interpd = interpd.view(N, W*H, C).sum(dim=1)
         summary = self.summarizer(interpd)
         return convout, summary
