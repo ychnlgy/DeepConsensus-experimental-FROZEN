81d483e2db14ade24212613de530ae09f40bfd64
Yuchen Li <ychnlgy@gmail.com>
2018-10-13 23:43:01 -0400

    Trying space agnostic pooling

--- src/config/Model0.py
+++ src/config/Model0.py
@@ -9,22 +9,18 @@ class Model(Base):
     def create_net(self, channels, classes):
         return torch.nn.Sequential(
         
-            models.ResNet(
-                kernelseq = [3, 3],
-                headsize = channels,
-                bodysize = 128,
-                tailsize = 128,
-                layers = 8
-            ),
+            
             
             models.DistillNet(
                 
                 # 28 -> 28
                 models.DistillLayer(
-                    convlayer = torch.nn.Sequential(
-                        torch.nn.Conv2d(128, 64, 3, padding=1, groups=64),
-                        torch.nn.LeakyReLU(),
-                        torch.nn.BatchNorm2d(64)
+                    convlayer = models.ResNet(
+                        kernelseq = [3, 3],
+                        headsize = channels,
+                        bodysize = 128,
+                        tailsize = 64,
+                        layers = 8
                     ),
                     interpreter = models.DenseNet(
                         headsize = 64,
@@ -42,29 +38,6 @@ class Model(Base):
                 ),
                 
                 # 28 -> 14
-                models.DistillLayer(
-                    convlayer = torch.nn.Sequential(
-                        torch.nn.Conv2d(64, 64, 3, padding=1, groups=64),
-                        torch.nn.MaxPool2d(2),
-                        torch.nn.LeakyReLU(),
-                        torch.nn.BatchNorm2d(64)
-                    ),
-                    interpreter = models.DenseNet(
-                        headsize = 64,
-                        bodysize = 64,
-                        tailsize = 128,
-                        layers = 1,
-                        dropout = 0.2
-                    ),
-                    summarizer = models.DenseNet(
-                        headsize = 128,
-                        bodysize = 32,
-                        tailsize = 32,
-                        layers = 1
-                    )
-                ),
-                
-                # 14 -> 7
                 models.DistillLayer(
                     convlayer = torch.nn.Sequential(
                         torch.nn.Conv2d(64, 32, 3, padding=1, groups=32),
@@ -87,17 +60,17 @@ class Model(Base):
                     )
                 ),
                 
-                # 7 -> 4
+                # 14 -> 7
                 models.DistillLayer(
                     convlayer = torch.nn.Sequential(
-                        torch.nn.Conv2d(32, 32, 3, padding=1, groups=32),
-                        torch.nn.AvgPool2d(3, padding=1, stride=2),
+                        torch.nn.Conv2d(32, 16, 3, padding=1, groups=16),
+                        torch.nn.MaxPool2d(2),
                         torch.nn.LeakyReLU(),
-                        torch.nn.BatchNorm2d(32)
+                        torch.nn.BatchNorm2d(16)
                     ),
                     interpreter = models.DenseNet(
-                        headsize = 32,
-                        bodysize = 32,
+                        headsize = 16,
+                        bodysize = 16,
                         tailsize = 32,
                         layers = 1,
                         dropout = 0.2
@@ -110,12 +83,35 @@ class Model(Base):
                     )
                 ),
                 
+                # 7 -> 4
+                models.DistillLayer(
+                    convlayer = torch.nn.Sequential(
+                        torch.nn.Conv2d(16, 8, 3, padding=1, groups=8),
+                        torch.nn.AvgPool2d(3, padding=1, stride=2),
+                        torch.nn.LeakyReLU(),
+                        torch.nn.BatchNorm2d(8)
+                    ),
+                    interpreter = models.DenseNet(
+                        headsize = 8,
+                        bodysize = 8,
+                        tailsize = 16,
+                        layers = 1,
+                        dropout = 0.2
+                    ),
+                    summarizer = models.DenseNet(
+                        headsize = 16,
+                        bodysize = 2,
+                        tailsize = 2,
+                        layers = 1
+                    )
+                ),
+                
                 
             ),
             
             models.DenseNet(
-                headsize = 32 + 32 + 16 + 8,
-                bodysize = 128,
+                headsize = 32 + 16 + 8 + 2,
+                bodysize = 64,
                 tailsize = classes,
                 layers = 2
             )
