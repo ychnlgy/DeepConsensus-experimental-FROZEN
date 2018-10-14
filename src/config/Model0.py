c84b4fb71f19d8abe975334a939d9b988f13d5b1
Yuchen Li <ychnlgy@gmail.com>
2018-10-14 15:25:04 -0400

    Reverting to 65% model

--- src/config/Model0.py
+++ src/config/Model0.py
@@ -16,81 +16,81 @@ class Model(Base):
                     convlayer = models.ResNet(
                         kernelseq = [3, 3],
                         headsize = channels,
-                        bodysize = 64,
-                        tailsize = 64,
+                        bodysize = 256,
+                        tailsize = 256,
                         layers = 8
                     ),
                     dropout = 0.2,
                     interpreter = models.DenseNet(
-                        headsize = 64,
-                        bodysize = 128,
-                        tailsize = 32,
-                        layers = 3,
+                        headsize = 256,
+                        bodysize = 256,
+                        tailsize = 128,
+                        layers = 2,
                         dropout = 0.2
                     ),
                     summarizer = models.DenseNet(
-                        headsize = 32,
-                        bodysize = 16,
-                        tailsize = 16,
-                        layers = 1,
-                        dropout = 0.0
+                        headsize = 128,
+                        bodysize = 256,
+                        tailsize = 64,
+                        layers = 2,
+                        dropout = 0.2
                     ),
                 ),
             
                 # 28 -> 14
                 models.DistillLayer(
                     convlayer = torch.nn.Sequential(
-                        torch.nn.Conv2d(32, 64, 3, padding=1, groups=32),
+                        torch.nn.Conv2d(128, 128, 3, padding=1, groups=128),
                         torch.nn.MaxPool2d(2),
                         torch.nn.LeakyReLU(),
-                        torch.nn.BatchNorm2d(64),
+                        torch.nn.BatchNorm2d(128),
                     ),
                     dropout = 0.2,
                     interpreter = models.DenseNet(
-                        headsize = 64,
-                        bodysize = 128,
-                        tailsize = 32,
-                        layers = 3,
+                        headsize = 128,
+                        bodysize = 256,
+                        tailsize = 128,
+                        layers = 2,
                         dropout = 0.2
                     ),
                     summarizer = models.DenseNet(
-                        headsize = 32,
-                        bodysize = 16,
-                        tailsize = 16,
-                        layers = 1,
-                        dropout = 0.0
+                        headsize = 128,
+                        bodysize = 64,
+                        tailsize = 32,
+                        layers = 2,
+                        dropout = 0.2
                     ),
                 ),
                     
                 # 14 -> 7
                 models.DistillLayer(
                     convlayer = torch.nn.Sequential(
-                        torch.nn.Conv2d(32, 64, 3, padding=1, groups=32),
+                        torch.nn.Conv2d(128, 128, 3, padding=1, groups=128),
                         torch.nn.MaxPool2d(2),
                         torch.nn.LeakyReLU(),
-                        torch.nn.BatchNorm2d(64),
+                        torch.nn.BatchNorm2d(128),
                     ),
                     dropout = 0.2,
                     interpreter = models.DenseNet(
-                        headsize = 64,
+                        headsize = 128,
                         bodysize = 128,
-                        tailsize = 32,
-                        layers = 3,
+                        tailsize = 64,
+                        layers = 2,
                         dropout = 0.2
                     ),
                     summarizer = models.DenseNet(
-                        headsize = 32,
-                        bodysize = 8,
-                        tailsize = 8,
-                        layers = 1,
-                        dropout = 0.0
+                        headsize = 64,
+                        bodysize = 32,
+                        tailsize = 16,
+                        layers = 2,
+                        dropout = 0.2
                     )
                 ),
                 
                 # 7 -> 4
                 models.DistillLayer(
                     convlayer = torch.nn.Sequential(
-                        torch.nn.Conv2d(32, 64, 3, padding=1, groups=32),
+                        torch.nn.Conv2d(64, 64, 3, padding=1, groups=64),
                         torch.nn.AvgPool2d(3, padding=1, stride=2),
                         torch.nn.LeakyReLU(),
                         torch.nn.BatchNorm2d(64),
@@ -98,27 +98,27 @@ class Model(Base):
                     dropout = 0.2,
                     interpreter = models.DenseNet(
                         headsize = 64,
-                        bodysize = 64,
-                        tailsize = 16,
+                        bodysize = 128,
+                        tailsize = 64,
                         layers = 2,
                         dropout = 0.2
                     ),
                     summarizer = models.DenseNet(
-                        headsize = 16,
-                        bodysize = 2,
-                        tailsize = 2,
-                        layers = 1,
-                        dropout = 0.0
+                        headsize = 64,
+                        bodysize = 32,
+                        tailsize = 4,
+                        layers = 2,
+                        dropout = 0.2
                     )
                 ),
                 
             ),
             
             models.DenseNet(
-                headsize = 16 + 16 + 8 + 2,
-                bodysize = 64,
+                headsize = 64 + 32 + 16 + 4,
+                bodysize = 256,
                 tailsize = classes,
                 layers = 2,
-                dropout = 0.1
+                dropout = 0.2
             )
         )
