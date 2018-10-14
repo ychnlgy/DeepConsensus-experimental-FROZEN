6cacfc12467807bc833322327daa26841e54582c
Yuchen Li <ychnlgy@gmail.com>
2018-10-14 13:02:13 -0400

    Trying to pass interpreted channels to future convs

--- src/config/Model0.py
+++ src/config/Model0.py
@@ -24,12 +24,12 @@ class Model(Base):
                     interpreter = models.DenseNet(
                         headsize = 64,
                         bodysize = 128,
-                        tailsize = 128,
+                        tailsize = 64,
                         layers = 2,
                         dropout = 0.2
                     ),
                     summarizer = models.DenseNet(
-                        headsize = 128,
+                        headsize = 64,
                         bodysize = 128,
                         tailsize = 32,
                         layers = 2,
@@ -49,12 +49,12 @@ class Model(Base):
                     interpreter = models.DenseNet(
                         headsize = 32,
                         bodysize = 64,
-                        tailsize = 64,
+                        tailsize = 32,
                         layers = 2,
                         dropout = 0.2
                     ),
                     summarizer = models.DenseNet(
-                        headsize = 64,
+                        headsize = 32,
                         bodysize = 64,
                         tailsize = 16,
                         layers = 2,
@@ -74,12 +74,12 @@ class Model(Base):
                     interpreter = models.DenseNet(
                         headsize = 16,
                         bodysize = 64,
-                        tailsize = 64,
+                        tailsize = 32,
                         layers = 1,
                         dropout = 0.2
                     ),
                     summarizer = models.DenseNet(
-                        headsize = 64,
+                        headsize = 32,
                         bodysize = 8,
                         tailsize = 8,
                         layers = 1,
@@ -90,15 +90,15 @@ class Model(Base):
                 # 7 -> 4
                 models.DistillLayer(
                     convlayer = torch.nn.Sequential(
-                        torch.nn.Conv2d(16, 8, 3, padding=1, groups=8),
+                        torch.nn.Conv2d(32, 16, 3, padding=1, groups=16),
                         torch.nn.AvgPool2d(3, padding=1, stride=2),
                         torch.nn.LeakyReLU(),
-                        torch.nn.BatchNorm2d(8),
+                        torch.nn.BatchNorm2d(16),
                     ),
                     dropout = 0.2,
                     interpreter = models.DenseNet(
-                        headsize = 8,
-                        bodysize = 8,
+                        headsize = 16,
+                        bodysize = 32,
                         tailsize = 32,
                         layers = 1,
                         dropout = 0.2
