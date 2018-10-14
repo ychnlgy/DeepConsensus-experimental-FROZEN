37a52839fcd144c12c513f0e6c44b6f7005a41b3
Yuchen Li <ychnlgy@gmail.com>
2018-10-14 16:40:51 -0400

    Trying extreme bottleneck

--- src/config/Model0.py
+++ src/config/Model0.py
@@ -24,14 +24,14 @@ class Model(Base):
                     interpreter = models.DenseNet(
                         headsize = 64,
                         bodysize = 32,
-                        tailsize = 64,
+                        tailsize = 16,
                         layers = 2,
                         dropout = 0.2
                     ),
                     summarizer = models.DenseNet(
-                        headsize = 64,
-                        bodysize = 128,
-                        tailsize = 32,
+                        headsize = 16,
+                        bodysize = 32,
+                        tailsize = 16,
                         layers = 2,
                         dropout = 0.2
                     ),
@@ -40,7 +40,7 @@ class Model(Base):
                 # 28 -> 14
                 models.DistillLayer(
                     convlayer = torch.nn.Sequential(
-                        torch.nn.Conv2d(64, 32, 3, padding=1, groups=32),
+                        torch.nn.Conv2d(16, 32, 3, padding=1, groups=16),
                         torch.nn.MaxPool2d(2),
                         torch.nn.LeakyReLU(),
                         torch.nn.BatchNorm2d(32),
@@ -49,14 +49,14 @@ class Model(Base):
                     interpreter = models.DenseNet(
                         headsize = 32,
                         bodysize = 16,
-                        tailsize = 32,
+                        tailsize = 8,
                         layers = 2,
                         dropout = 0.2
                     ),
                     summarizer = models.DenseNet(
-                        headsize = 32,
-                        bodysize = 64,
-                        tailsize = 16,
+                        headsize = 8,
+                        bodysize = 32,
+                        tailsize = 8,
                         layers = 2,
                         dropout = 0.2
                     ),
@@ -115,7 +115,7 @@ class Model(Base):
             ),
             
             models.DenseNet(
-                headsize = 32 + 16,
+                headsize = 16 + 8,
                 bodysize = 64,
                 tailsize = classes,
                 layers = 2,
