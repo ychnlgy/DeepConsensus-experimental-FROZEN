8a9be387f6defcd5f72c933ac0919f9b8fcdb21b
Yuchen Li <ychnlgy@gmail.com>
2018-10-15 19:00:06 -0400

    Trying larger network

--- src/config/Model0.py
+++ src/config/Model0.py
@@ -16,39 +16,39 @@ class Model(Base):
                     convlayer = models.ResNet(
                         kernelseq = [3, 3],
                         headsize = channels,
-                        bodysize = 64,
-                        tailsize = 64,
+                        bodysize = 128,
+                        tailsize = 128,
                         layers = 8
                     ),
                     dropout = 0.2,
                     masker = models.DenseNet(
-                        headsize = 64,
+                        headsize = 128,
                         bodysize = 32,
                         tailsize = 1,
                         layers = 2,
-                        dropout = 0.0,
+                        dropout = 0.2,
                         activation = models.AbsTanh()
                     ),
                     interpreter = models.DenseNet(
-                        headsize = 64,
-                        bodysize = 64,
-                        tailsize = 64,
-                        layers = 1,
-                        dropout = 0.0,
+                        headsize = 128,
+                        bodysize = 256,
+                        tailsize = 128,
+                        layers = 2,
+                        dropout = 0.2,
                     ),
                     summarizer = models.DenseNet(
-                        headsize = 64,
+                        headsize = 128,
                         bodysize = 64,
                         tailsize = 32,
-                        layers = 1,
-                        dropout = 0.0,
+                        layers = 2,
+                        dropout = 0.2,
                     ),
                 ),
             
                 # 28 -> 14
                 models.DistillLayer(
                     convlayer = torch.nn.Sequential(
-                        torch.nn.Conv2d(64, 64, 3, padding=1, groups=64),
+                        torch.nn.Conv2d(128, 64, 3, padding=1, groups=64),
                         torch.nn.MaxPool2d(2),
                         torch.nn.LeakyReLU(),
                         torch.nn.BatchNorm2d(64),
@@ -59,22 +59,22 @@ class Model(Base):
                         bodysize = 32,
                         tailsize = 1,
                         layers = 2,
-                        dropout = 0.0,
+                        dropout = 0.2,
                         activation = models.AbsTanh()
                     ),
                     interpreter = models.DenseNet(
-                        headsize = 64,
-                        bodysize = 64,
-                        tailsize = 64,
-                        layers = 1,
-                        dropout = 0.0,
+                        headsize = 128,
+                        bodysize = 128,
+                        tailsize = 128,
+                        layers = 2,
+                        dropout = 0.2,
                     ),
                     summarizer = models.DenseNet(
-                        headsize = 64,
+                        headsize = 128,
                         bodysize = 64,
                         tailsize = 32,
-                        layers = 1,
-                        dropout = 0.0,
+                        layers = 2,
+                        dropout = 0.2,
                     ),
                 ),
                 
@@ -85,6 +85,6 @@ class Model(Base):
                 bodysize = 128,
                 tailsize = classes,
                 layers = 2,
-                dropout = 0.0
+                dropout = 0.2
             )
         )
