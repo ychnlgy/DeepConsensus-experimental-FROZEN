f07bbcdd115176bbc8e0ffcdfcbf9582c72c3eeb
Yuchen Li <ychnlgy@gmail.com>
2018-10-15 17:56:22 -0400

    Trying smaller model with abs instead of abstanh

--- src/config/Model0.py
+++ src/config/Model0.py
@@ -26,7 +26,7 @@ class Model(Base):
                         tailsize = 1,
                         layers = 2,
                         dropout = 0.2,
-                        activation = models.AbsTanh()
+                        activation = torch.abs
                     ),
                     dropout = 0.2,
                     interpreter = models.DenseNet(
@@ -59,7 +59,7 @@ class Model(Base):
                         tailsize = 1,
                         layers = 2,
                         dropout = 0.2,
-                        activation = models.AbsTanh()
+                        activation = torch.abs
                     ),
                     dropout = 0.2,
                     interpreter = models.DenseNet(
