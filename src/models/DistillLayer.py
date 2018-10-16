67169b6a55e33a2bc9694ae30f2b212e21e90bc0
Yuchen Li <ychnlgy@gmail.com>
2018-10-16 01:23:01 -0400

    Used heavier dense nets

--- src/models/DistillLayer.py
+++ src/models/DistillLayer.py
@@ -4,11 +4,10 @@ import misc
 
 class DistillLayer(torch.nn.Module):
 
-    def __init__(self, convlayer, masker, selector, dropout, interpreter, summarizer):
+    def __init__(self, convlayer, masker, dropout, interpreter, summarizer):
         super(DistillLayer, self).__init__()
         self.convlayer   = convlayer
         self.masker      = masker
-        self.selector    = selector
         self.interpreter = interpreter
         self.summarizer  = summarizer
         self.dropout     = torch.nn.Dropout2d(p=dropout)
@@ -20,8 +19,7 @@ class DistillLayer(torch.nn.Module):
         N, W, H, C = convinp.size()
         convinp = convinp.view(N, W*H, C)
         maskout = self.masker(convinp)
-        selects = self.selector(convinp)
-        interpd = self.interpreter(convinp * selects)
+        interpd = self.interpreter(convinp)
         spooled = (maskout * interpd).sum(dim=1)
         summary = self.summarizer(spooled)
         return convout, summary
