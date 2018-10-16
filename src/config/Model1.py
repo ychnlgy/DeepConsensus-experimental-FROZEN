ca50d4f305be433f887dcc327cb95954aee0a07a
Yuchen Li <ychnlgy@gmail.com>
2018-10-15 21:26:24 -0400

    Seeing how parts affect the layer

--- src/config/Model1.py
+++ src/config/Model1.py
@@ -30,14 +30,12 @@ class Model(Base):
             torch.nn.BatchNorm2d(64),
             
             # 7 -> 4
-            torch.nn.Conv2d(64, 64, 3, padding=1, groups=4),
-            torch.nn.AvgPool2d(3, padding=1, stride=2),
+            torch.nn.Conv2d(64, 64, 3, padding=1, groups=4, stride=2),
             torch.nn.LeakyReLU(),
             torch.nn.BatchNorm2d(64),
             
             # 4 -> 1
             torch.nn.Conv2d(64, 64, 4, padding=0, groups=4),
-            #torch.nn.AvgPool2d(4),
             torch.nn.LeakyReLU(),
             torch.nn.BatchNorm2d(64),
             
