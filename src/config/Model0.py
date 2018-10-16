0c31d245b4c809b256f0d20e1635e193b7b1832f
Yuchen Li <ychnlgy@gmail.com>
2018-10-16 17:24:32 -0400

    Trying distillations on all levels of ResNet

--- src/config/Model0.py
+++ src/config/Model0.py
@@ -13,8 +13,116 @@ class Model(Base):
         
             models.DistillNet(
             
-                # 28 -> 28
-                *[models.DistillLayer(
+                # 1
+                models.DistillLayer(
+                    dropout = 0.2,
+                    masker = models.DenseNet(
+                        headsize = 63,
+                        bodysize = 32,
+                        tailsize = 1,
+                        layers = 2,
+                        dropout = 0.2,
+                        activation = models.AbsTanh()
+                    ),
+                    interpreter = models.DenseNet(
+                        headsize = 63,
+                        bodysize = 64,
+                        tailsize = 64,
+                        layers = 2,
+                        dropout = 0.2
+                    ),
+                    summarizer = models.DenseNet(
+                        headsize = 64,
+                        bodysize = 4,
+                        tailsize = 4,
+                        layers = 1, # deactivated
+                        dropout = 0.2
+                    ),
+                ),
+                
+                # 2
+                models.DistillLayer(
+                    dropout = 0.2,
+                    masker = models.DenseNet(
+                        headsize = 63,
+                        bodysize = 32,
+                        tailsize = 1,
+                        layers = 2,
+                        dropout = 0.2,
+                        activation = models.AbsTanh()
+                    ),
+                    interpreter = models.DenseNet(
+                        headsize = 63,
+                        bodysize = 64,
+                        tailsize = 64,
+                        layers = 2,
+                        dropout = 0.2
+                    ),
+                    summarizer = models.DenseNet(
+                        headsize = 64,
+                        bodysize = 4,
+                        tailsize = 4,
+                        layers = 1, # deactivated
+                        dropout = 0.2
+                    ),
+                ),
+                
+                # 3
+                models.DistillLayer(
+                    dropout = 0.2,
+                    masker = models.DenseNet(
+                        headsize = 63,
+                        bodysize = 32,
+                        tailsize = 1,
+                        layers = 2,
+                        dropout = 0.2,
+                        activation = models.AbsTanh()
+                    ),
+                    interpreter = models.DenseNet(
+                        headsize = 63,
+                        bodysize = 64,
+                        tailsize = 64,
+                        layers = 2,
+                        dropout = 0.2
+                    ),
+                    summarizer = models.DenseNet(
+                        headsize = 64,
+                        bodysize = 8,
+                        tailsize = 8,
+                        layers = 1, # deactivated
+                        dropout = 0.2
+                    ),
+                ),
+                
+                # 4
+                models.DistillLayer(
+                    dropout = 0.2,
+                    masker = models.DenseNet(
+                        headsize = 63,
+                        bodysize = 32,
+                        tailsize = 1,
+                        layers = 2,
+                        dropout = 0.2,
+                        activation = models.AbsTanh()
+                    ),
+                    interpreter = models.DenseNet(
+                        headsize = 63,
+                        bodysize = 64,
+                        tailsize = 64,
+                        layers = 2,
+                        dropout = 0.2
+                    ),
+                    summarizer = models.DenseNet(
+                        headsize = 64,
+                        bodysize = 8,
+                        tailsize = 8,
+                        layers = 1, # deactivated
+                        dropout = 0.2
+                    ),
+                ),
+                
+                # 5
+                models.DistillLayer(
                     dropout = 0.2,
                     masker = models.DenseNet(
                         headsize = 63,
@@ -38,7 +146,88 @@ class Model(Base):
                         layers = 1, # deactivated
                         dropout = 0.2
                     ),
-                ) for i in range(LAYERS)],
+                ),
+                
+                # 6
+                models.DistillLayer(
+                    dropout = 0.2,
+                    masker = models.DenseNet(
+                        headsize = 63,
+                        bodysize = 32,
+                        tailsize = 1,
+                        layers = 2,
+                        dropout = 0.2,
+                        activation = models.AbsTanh()
+                    ),
+                    interpreter = models.DenseNet(
+                        headsize = 63,
+                        bodysize = 64,
+                        tailsize = 64,
+                        layers = 2,
+                        dropout = 0.2
+                    ),
+                    summarizer = models.DenseNet(
+                        headsize = 64,
+                        bodysize = 16,
+                        tailsize = 16,
+                        layers = 1, # deactivated
+                        dropout = 0.2
+                    ),
+                ),
+                
+                # 7
+                models.DistillLayer(
+                    dropout = 0.2,
+                    masker = models.DenseNet(
+                        headsize = 63,
+                        bodysize = 32,
+                        tailsize = 1,
+                        layers = 2,
+                        dropout = 0.2,
+                        activation = models.AbsTanh()
+                    ),
+                    interpreter = models.DenseNet(
+                        headsize = 63,
+                        bodysize = 64,
+                        tailsize = 64,
+                        layers = 2,
+                        dropout = 0.2
+                    ),
+                    summarizer = models.DenseNet(
+                        headsize = 64,
+                        bodysize = 32,
+                        tailsize = 32,
+                        layers = 1, # deactivated
+                        dropout = 0.2
+                    ),
+                ),
+                
+                # 8
+                models.DistillLayer(
+                    dropout = 0.2,
+                    masker = models.DenseNet(
+                        headsize = 63,
+                        bodysize = 32,
+                        tailsize = 1,
+                        layers = 2,
+                        dropout = 0.2,
+                        activation = models.AbsTanh()
+                    ),
+                    interpreter = models.DenseNet(
+                        headsize = 63,
+                        bodysize = 64,
+                        tailsize = 64,
+                        layers = 2,
+                        dropout = 0.2
+                    ),
+                    summarizer = models.DenseNet(
+                        headsize = 64,
+                        bodysize = 32,
+                        tailsize = 32,
+                        layers = 1, # deactivated
+                        dropout = 0.2
+                    ),
+                ),
                 
                 iternet = models.ResNet(
                     kernelseq = [3, 3],
@@ -51,8 +240,8 @@ class Model(Base):
             ),
             
             models.DenseNet(
-                headsize = 16 * 8,
-                bodysize = 32,
+                headsize = 4*2 + 8*2 + 16*2 + 32*2,
+                bodysize = 64,
                 tailsize = classes,
                 layers = 2,
                 dropout = 0.2
