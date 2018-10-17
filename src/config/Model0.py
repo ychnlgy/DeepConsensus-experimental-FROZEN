ff2c9f0618df339c190449fd0bfce42f52ac6bad
Yuchen Li <ychnlgy@gmail.com>
2018-10-17 19:57:59 -0400

    Trying no resent

--- src/config/Model0.py
+++ src/config/Model0.py
@@ -11,21 +11,23 @@ class Model(Base):
         
             models.DistillNet(
             
-                encoder = torch.nn.GRU(
-                    input_size = 32,
-                    hidden_size = 64,
-                    num_layers = 2,
-                    dropout = 0.2
-                ),
+#                encoder = torch.nn.GRU(
+#                    input_size = 32,
+#                    hidden_size = 64,
+#                    num_layers = 2,
+#                    dropout = 0.2
+#                ),
             
                 layers = [
                 
                     # 28 -> 28
                     models.DistillLayer(
-                        convlayer = torch.nn.Sequential(
-                            torch.nn.Conv2d(channels, 64, 3, padding=1),
-                            torch.nn.LeakyReLU(),
-                            torch.nn.BatchNorm2d(64)
+                        convlayer = models.ResNet(
+                            kernelseq = [3, 3],
+                            headsize = channels,
+                            bodysize = 63,
+                            tailsize = 64,
+                            layers = 8
                         ),
                         dropout = 0.0,
                         masker = models.DenseNet(
@@ -38,72 +40,75 @@ class Model(Base):
                         ),
                         interpreter = models.DenseNet(
                             headsize = 64,
-                            bodysize = 32,
-                            tailsize = 32,
+                            bodysize = 128,
+                            tailsize = 128,
                             layers = 1,
-                            dropout = 0.0
+                            dropout = 0.2
                         )
                     ),
                     
-                    # 28 -> 28
-                    models.DistillLayer(
-                        convlayer = torch.nn.Sequential(
-                            torch.nn.Conv2d(64, 64, 3, padding=1, groups=64),
-                            torch.nn.LeakyReLU(),
-                            torch.nn.BatchNorm2d(64)
-                        ),
-                        dropout = 0.0,
-                        masker = models.DenseNet(
-                            headsize = 64,
-                            bodysize = 32,
-                            tailsize = 1,
-                            layers = 2,
-                            dropout = 0.2,
-                            activation = models.AbsTanh()
-                        ),
-                        interpreter = models.DenseNet(
-                            headsize = 64,
-                            bodysize = 32,
-                            tailsize = 32,
-                            layers = 1,
-                            dropout = 0.0
-                        )
-                    ),
-                    
-                    # 28 -> 28
-                    models.DistillLayer(
-                        convlayer = torch.nn.Sequential(
-                            torch.nn.Conv2d(64, 64, 3, padding=1, groups=64),
-                            torch.nn.LeakyReLU(),
-                            torch.nn.BatchNorm2d(64)
-                        ),
-                        dropout = 0.0,
-                        masker = models.DenseNet(
-                            headsize = 64,
-                            bodysize = 32,
-                            tailsize = 1,
-                            layers = 2,
-                            dropout = 0.2,
-                            activation = models.AbsTanh()
-                        ),
-                        interpreter = models.DenseNet(
-                            headsize = 64,
-                            bodysize = 32,
-                            tailsize = 32,
-                            layers = 1,
-                            dropout = 0.0
-                        )
-                    ),
+#                    # 28 -> 14
+#                    models.DistillLayer(
+#                        convlayer = torch.nn.Sequential(
+#                            torch.nn.Conv2d(63, 32, 3, padding=1, groups=32),
+#                            torch.nn.MaxPool2d(2),
+#                            torch.nn.LeakyReLU(),
+#                            torch.nn.BatchNorm2d(32)
+#                        ),
+#                        dropout = 0.0,
+#                        masker = models.DenseNet(
+#                            headsize = 32,
+#                            bodysize = 16,
+#                            tailsize = 1,
+#                            layers = 2,
+#                            dropout = 0.2,
+#                            activation = models.AbsTanh()
+#                        ),
+#                        interpreter = models.DenseNet(
+#                            headsize = 32,
+#                            bodysize = 32,
+#                            tailsize = 32,
+#                            layers = 1,
+#                            dropout = 0.0
+#                        )
+#                    ),
+#                    
+#                    # 14 -> 14
+#                    models.DistillLayer(
+#                        convlayer = torch.nn.Sequential(
+#                            torch.nn.Conv2d(32, 32, 3, padding=1, groups=32),
+#                            torch.nn.LeakyReLU(),
+#                            torch.nn.BatchNorm2d(32)
+#                        ),
+#                        dropout = 0.0,
+#                        masker = models.DenseNet(
+#                            headsize = 32,
+#                            bodysize = 16,
+#                            tailsize = 1,
+#                            layers = 2,
+#                            dropout = 0.2,
+#                            activation = models.AbsTanh()
+#                        ),
+#                        interpreter = models.DenseNet(
+#                            headsize = 32,
+#                            bodysize = 32,
+#                            tailsize = 32,
+#                            layers = 1,
+#                            dropout = 0.0
+#                        )
+#                    ),
                     
                 ],
                 
             ),
             
             models.DenseNet(
-                headsize = 64,
-                bodysize = 32,
+                headsize = 128,
+                bodysize = 64,
                 tailsize = classes,
                 layers = 2,
-                dropout = 0.0
-            )
+                dropout = 0.2
+            ),
+            
+            models.Reshape(classes)
         )
--- src/models/DistillNet.py
+++ src/models/DistillNet.py
@@ -4,21 +4,20 @@ import misc
 
 class DistillNet(torch.nn.Module):
 
-    def __init__(self, encoder, layers, iternet=None):
+    def __init__(self, layers):
         super(DistillNet, self).__init__()
         
-        self.encoder = encoder
+        #self.encoder = encoder
         
-        if iternet is not None:
-            self.generate_vecs = self.generate_vecs_iter
-            self.iternet = iternet
+#        if iternet is not None:
+#            self.generate_vecs = self.generate_vecs_iter
+#            self.iternet = iternet
         
         self.layers = torch.nn.ModuleList(layers)
     
     def forward(self, X):
         vecs = misc.util.reverse_iterator(self.generate_vecs(X))
         vecs = torch.stack(vecs) # layers, N, C
-        encd, state = self.encoder(vecs)
         return state[0] # N, C'
     
     def generate_vecs(self, X):
@@ -26,8 +25,8 @@ class DistillNet(torch.nn.Module):
             X, vec = layer(X)
             yield vec
     
-    def generate_vecs_iter(self, X):
-        Xs = self.iternet.iter_forward(X)
-        for i, (output, layer) in enumerate(zip(Xs, self.layers)):
-            yield layer(output)[1]
-        assert i + 1 == len(self.layers)
+#    def generate_vecs_iter(self, X):
+#        Xs = self.iternet.iter_forward(X)
+#        for i, (output, layer) in enumerate(zip(Xs, self.layers)):
+#            yield layer(output)[1]
+#        assert i + 1 == len(self.layers)
