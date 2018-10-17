8f3dc8eebfe121da2f46f0736e4cca2b54c3a1b9
Yuchen Li <ychnlgy@gmail.com>
2018-10-17 19:23:05 -0400

    Trying no resent

--- src/config/Model0.py
+++ src/config/Model0.py
@@ -45,6 +45,56 @@ class Model(Base):
                         )
                     ),
                     
+                    # 28 -> 28
+                    models.DistillLayer(
+                        convlayer = torch.nn.Sequential(
+                            torch.nn.Conv2d(64, 64, 3, padding=1, groups=64),
+                            torch.nn.LeakyReLU(),
+                            torch.nn.BatchNorm2d(64)
+                        ),
+                        dropout = 0.0,
+                        masker = models.DenseNet(
+                            headsize = 64,
+                            bodysize = 32,
+                            tailsize = 1,
+                            layers = 2,
+                            dropout = 0.2,
+                            activation = models.AbsTanh()
+                        ),
+                        interpreter = models.DenseNet(
+                            headsize = 64,
+                            bodysize = 32,
+                            tailsize = 32,
+                            layers = 1,
+                            dropout = 0.0
+                        )
+                    ),
+                    
+                    # 28 -> 28
+                    models.DistillLayer(
+                        convlayer = torch.nn.Sequential(
+                            torch.nn.Conv2d(64, 64, 3, padding=1, groups=64),
+                            torch.nn.LeakyReLU(),
+                            torch.nn.BatchNorm2d(64)
+                        ),
+                        dropout = 0.0,
+                        masker = models.DenseNet(
+                            headsize = 64,
+                            bodysize = 32,
+                            tailsize = 1,
+                            layers = 2,
+                            dropout = 0.2,
+                            activation = models.AbsTanh()
+                        ),
+                        interpreter = models.DenseNet(
+                            headsize = 64,
+                            bodysize = 32,
+                            tailsize = 32,
+                            layers = 1,
+                            dropout = 0.0
+                        )
+                    ),
+                    
                     # 28 -> 14
                     models.DistillLayer(
                         convlayer = torch.nn.Sequential(
@@ -72,6 +122,57 @@ class Model(Base):
                     ),
                     
                     # 14 -> 14
+                    models.DistillLayer(
+                        convlayer = torch.nn.Sequential(
+                            torch.nn.Conv2d(64, 64, 3, padding=1, groups=64),
+                            torch.nn.LeakyReLU(),
+                            torch.nn.BatchNorm2d(64)
+                        ),
+                        dropout = 0.0,
+                        masker = models.DenseNet(
+                            headsize = 64,
+                            bodysize = 32,
+                            tailsize = 1,
+                            layers = 2,
+                            dropout = 0.2,
+                            activation = models.AbsTanh()
+                        ),
+                        interpreter = models.DenseNet(
+                            headsize = 64,
+                            bodysize = 32,
+                            tailsize = 32,
+                            layers = 1,
+                            dropout = 0.0
+                        )
+                    ),
+                    
+                    # 14 -> 7
+                    models.DistillLayer(
+                        convlayer = torch.nn.Sequential(
+                            torch.nn.Conv2d(64, 64, 3, padding=1, groups=64),
+                            torch.nn.MaxPool2d(2),
+                            torch.nn.LeakyReLU(),
+                            torch.nn.BatchNorm2d(64)
+                        ),
+                        dropout = 0.0,
+                        masker = models.DenseNet(
+                            headsize = 64,
+                            bodysize = 32,
+                            tailsize = 1,
+                            layers = 2,
+                            dropout = 0.2,
+                            activation = models.AbsTanh()
+                        ),
+                        interpreter = models.DenseNet(
+                            headsize = 64,
+                            bodysize = 32,
+                            tailsize = 32,
+                            layers = 1,
+                            dropout = 0.0
+                        )
+                    ),
+                    
+                    # 7 -> 7
                     models.DistillLayer(
                         convlayer = torch.nn.Sequential(
                             torch.nn.Conv2d(64, 64, 3, padding=1, groups=64),
