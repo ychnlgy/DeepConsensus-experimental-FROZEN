b44a0933fd02f9721788ec64bbc086ae49c88d1a
Yuchen Li <ychnlgy@gmail.com>
2018-10-18 18:18:11 -0400

    Trying MaxLoss

--- src/misc/matrix.py
+++ src/misc/matrix.py
@@ -1,2 +1,7 @@
 def apply_permutation(module, X, permutation):
     return module(X.permute(permutation)).permute(permutation)
+
+def number(vec):
+    ind = torch.arange(len(vec))
+    vec = torch.stack(ind, vec)
+    return vec.transpose(0, 1)
--- src/models/MaxLoss.py
+++ src/models/MaxLoss.py
@@ -1,9 +1,12 @@
 import torch
 
+import misc
+
 class MaxLoss(torch.nn.CrossEntropyLoss):
 
     def forward(self, yh, y):
         vals, indx = yh.max(dim=1)
+        indx = misc.matrix.number(indx)
         blank = torch.zeros_like(yh, requires_grad=True)
         blank[indx] = vals
         return super(MaxLoss, self).forward(blank, y)
