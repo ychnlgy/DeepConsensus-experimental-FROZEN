e655889fba537d0e4e52ebdd44877cdfd25c1df5
Yuchen Li <ychnlgy@gmail.com>
2018-10-18 18:24:52 -0400

    Trying MaxLoss

--- src/models/MaxLoss.py
+++ src/models/MaxLoss.py
@@ -7,6 +7,6 @@ class MaxLoss(torch.nn.CrossEntropyLoss):
     def forward(self, yh, y):
         vals, indx = yh.max(dim=1)
         batch = torch.arange(len(yh)).to(yh.device)
-        blank = torch.zeros_like(yh, requires_grad=True).to(yh.device)
+        blank = torch.zeros_like(yh).to(yh.device)
         blank[batch, indx] = vals
         return super(MaxLoss, self).forward(blank, y)
