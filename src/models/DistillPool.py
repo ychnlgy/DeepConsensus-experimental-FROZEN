429095c49085534b60a93ebcba1397efd7413ddb
Yuchen Li <ychnlgy@gmail.com>
2018-10-18 15:30:30 -0400

    Trying no resnet

--- src/models/DistillPool.py
+++ src/models/DistillPool.py
@@ -35,5 +35,5 @@ class DistillPool(torch.nn.Module):
         N, C, W, H = X.size()
         X = X.permute(0, 2, 3, 1).view(N, W*H, C)
         #w = self.g(X)
-        #v = self.h(X) * w
-        return X.sum(dim=1)
+        v = self.h(X)# * w
+        return v.sum(dim=1)
