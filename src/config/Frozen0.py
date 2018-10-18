25b882e5076ee79cdda311d062fb288197ffe44e
Yuchen Li <ychnlgy@gmail.com>
2018-10-18 15:15:05 -0400

    Trying no resnet

--- src/config/Frozen0.py
+++ src/config/Frozen0.py
@@ -55,10 +55,6 @@ class Model(Base):
             torch.nn.LeakyReLU(),
             torch.nn.BatchNorm2d(64),
             
-            torch.nn.Conv2d(64, 64, 3, padding=1, groups=64),
-            torch.nn.LeakyReLU(),
-            torch.nn.BatchNorm2d(64),
-            
             models.DistillPool(
             
                 g = models.DenseNet(
