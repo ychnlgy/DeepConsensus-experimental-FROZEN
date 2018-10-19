44ebf311fc794ece99fd84f2c740a6f714708931
Yuchen Li <ychnlgy@gmail.com>
2018-10-19 17:58:39 -0400

    Trying reconstructor

--- src/train.py
+++ src/train.py
@@ -466,6 +466,9 @@ def main(dataset, modelf, classic=0, trainbatch=100, testbatch=300, cycle=10, da
             print_(" -- <VERR> %.3f" % w, silent)
             
             if w < lowest:
+                
+                print("Saving to %s..." % modelf)
+            
                 lowest = w
                 model.save(modelf)
                 reconstructor.save(modelf + "-r")
