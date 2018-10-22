00899c662e3f332e8c964a8fa4b6a6925c3790d2
Yuchen Li <ychnlgy@gmail.com>
2018-10-22 16:27:57 -0400

    Trying full role-classification

--- src/models/ChannelClassifier.py
+++ src/models/ChannelClassifier.py
@@ -19,6 +19,6 @@ class ChannelClassifier(Homogenizer):
         N, C, W, H = X.size()
         X = X.permute(0, 2, 3, 1).contiguous().view(N*W*H, C)
         U = super(ChannelClassifier, self).forward(X) # N*W*H, C'
-        Nh, Ch = W.size()
+        Nh, Ch = U.size()
         U = U.view(N, W, H, Ch).permute(0, 3, 1, 2)
         return U
