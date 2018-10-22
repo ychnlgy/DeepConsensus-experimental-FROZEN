0b0e052ac2a09f0e75e6005b6292908040b89c41
Yuchen Li <ychnlgy@gmail.com>
2018-10-22 17:32:24 -0400

    Trying full role-classification

--- src/model.py
+++ src/model.py
@@ -17,46 +17,49 @@ class Model(models.Savable):
             torch.nn.BatchNorm2d(64),
             
             # 28 -> 14
+            models.InteractionObserver(),
             models.ChannelTransform(
-                headsize = 64,
-                bodysize = 128,
-                tailsize = 64,
-                layers = 2,
-                dropout = 0.2
+                headsize = 9,
+                bodysize = 32,
+                tailsize = 32,
+                layers = 1,
+                #dropout = 0.2
             ),
             models.SoftmaxCombine(),
 #            models.ChannelClassifier(
 #                hiddensize = 64,
 #                classes = 16
 #            ),
-            torch.nn.Conv2d(64, 64, 3, padding=1),
+            torch.nn.Conv2d(32, 32, 3, padding=1),
             torch.nn.LeakyReLU(),
-            torch.nn.BatchNorm2d(64),
+            torch.nn.BatchNorm2d(32),
             
             # 14 -> 7
+            models.InteractionObserver(),
             models.ChannelTransform(
-                headsize = 64,
-                bodysize = 128,
-                tailsize = 64,
-                layers = 2,
-                dropout = 0.2
+                headsize = 9,
+                bodysize = 32,
+                tailsize = 32,
+                layers = 1,
+                #dropout = 0.2
             ),
             models.SoftmaxCombine(),
 #            models.ChannelClassifier(
 #                hiddensize = 64,
 #                classes = 16
 #            ),
-            torch.nn.Conv2d(64, 64, 3, padding=1),
+            torch.nn.Conv2d(32, 32, 3, padding=1),
             torch.nn.LeakyReLU(),
-            torch.nn.BatchNorm2d(64),
+            torch.nn.BatchNorm2d(32),
             
             # 7 -> 4
+            models.InteractionObserver(),
             models.ChannelTransform(
-                headsize = 64,
-                bodysize = 128,
-                tailsize = 64,
-                layers = 2,
-                dropout = 0.2
+                headsize = 9,
+                bodysize = 32,
+                tailsize = 32,
+                layers = 1,
+                #dropout = 0.2
             ),
             models.SoftmaxCombine(kernel=3, padding=1, stride=2),
 #            models.ChannelClassifier(
@@ -64,40 +67,41 @@ class Model(models.Savable):
 #                classes = 16
 #            ),
             
-            torch.nn.Conv2d(64, 64, 3, padding=1),
+            torch.nn.Conv2d(32, 32, 3, padding=1),
             torch.nn.LeakyReLU(),
-            torch.nn.BatchNorm2d(64),
+            torch.nn.BatchNorm2d(32),
             
             # 4 -> 2
+            models.InteractionObserver(),
             models.ChannelTransform(
-                headsize = 64,
-                bodysize = 128,
-                tailsize = 64,
-                layers = 2,
-                dropout = 0.2
+                headsize = 9,
+                bodysize = 32,
+                tailsize = 32,
+                layers = 1,
+                #dropout = 0.2
             ),
             models.SoftmaxCombine(),
-            models.ChannelClassifier(
-                hiddensize = 64,
-                classes = 16
-            ),
-            
-            torch.nn.Conv2d(64, 64, 3, padding=1),
+#            models.ChannelClassifier(
+#                hiddensize = 64,
+#                classes = 16
+#            ),
+            torch.nn.Conv2d(32, 32, 3, padding=1),
             torch.nn.LeakyReLU(),
-            torch.nn.BatchNorm2d(64),
+            torch.nn.BatchNorm2d(32),
             
             # 2 -> 1
+            models.InteractionObserver(),
             models.ChannelTransform(
-                headsize = 64,
-                bodysize = 128,
-                tailsize = 64,
-                layers = 2,
-                dropout = 0.2
+                headsize = 9,
+                bodysize = 32,
+                tailsize = 32,
+                layers = 1,
+                #dropout = 0.2
             ),
             models.SoftmaxCombine(),
             
-            models.Reshape(64),
-            models.Classifier(hiddensize=64, classes=classes)
+            models.Reshape(32),
+            models.Classifier(hiddensize=32, classes=classes)
         )
     
     def forward(self, X):
