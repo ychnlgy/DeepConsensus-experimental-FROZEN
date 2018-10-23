937a68a59645fa61ebc525669799c524ba0280bd
Yuchen Li <ychnlgy@gmail.com>
2018-10-23 13:39:59 -0400

    Trying distill

--- src/models/Classifier.py
+++ src/models/Classifier.py
@@ -7,7 +7,7 @@ class Classifier(torch.nn.Module):
     def __init__(self, hiddensize, classes):
         super(Classifier, self).__init__()
         self.grp = torch.nn.Parameter(torch.rand(classes, hiddensize))
-        self.dif = models.Norm()
+        self.dif = models.Norm(p=1)
     
     def forward(self, X):
         return torch.exp(-self.dif(X, self.grp))
