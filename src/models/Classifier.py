e95a7162e0f9085d8f907a84e3fcf34d1e09697d
Yuchen Li <ychnlgy@gmail.com>
2018-10-23 13:38:21 -0400

    Trying distill

--- src/models/Classifier.py
+++ src/models/Classifier.py
@@ -8,7 +8,6 @@ class Classifier(torch.nn.Module):
         super(Classifier, self).__init__()
         self.grp = torch.nn.Parameter(torch.rand(classes, hiddensize))
         self.dif = models.Norm()
-        self.max = torch.nn.Softmax(dim=1)
     
     def forward(self, X):
-        return self.max(-self.dif(X, self.grp))
+        return torch.exp(-self.dif(X, self.grp))
