ade4cc169008102f0083350c26418dcf46f86f3a
Yuchen Li <ychnlgy@gmail.com>
2018-10-23 15:20:55 -0400

    Norms alongside cosine similarity

--- src/models/Classifier.py
+++ src/models/Classifier.py
@@ -8,12 +8,13 @@ class Classifier(torch.nn.Module):
         super(Classifier, self).__init__()
         self.grp = torch.nn.Parameter(torch.rand(classes, hiddensize))
         self.cos = models.CosineSimilarity()
+        self.dif = models.Norm(p=2)
         self.max = torch.nn.Softmax(dim=1)
     
     def forward(self, X):
         cs = self.cos(X, self.grp)
         confidence, indices = cs.max(dim=1)
         confidence = confidence.view(-1, 1)
-        confusion = self.max(cs)
+        confusion = self.max(self.dif(X, self.grp))
         assert len(confidence) == len(confusion)
         return confidence * confusion
--- src/models/Norm.py
+++ src/models/Norm.py
@@ -35,32 +35,3 @@ class Norm(torch.nn.Module):
 
         diff = (vectors - targets).norm(p=self.p, dim=1)
         return diff.view(N, C)
-    
-    @staticmethod
-    def unittest():
-        
-        cosine = CosineSimilarity()
-        
-        vectors = torch.Tensor([
-            [1, 1, 1],
-            [2, 2, 2],
-            [1, 0, 0],
-            [2, 0, 0],
-            [3, 4, 5],
-            [9, 16, 25]
-        ])
-        
-        targets = torch.Tensor([
-            [5, 0, 0],
-            [81, 256, 625],
-            [-5, -5, -5]
-        ])
-        
-        assert (cosine(vectors, targets) - torch.Tensor([
-            [ 0.5774,  0.8165, -1.0000],
-            [ 0.5774,  0.8165, -1.0000],
-            [ 1.0000,  0.1191, -0.5774],
-            [ 1.0000,  0.1191, -0.5774],
-            [ 0.4243,  0.9131, -0.9798],
-            [ 0.2902,  0.9693, -0.9307]
-        ])).norm().item() < 1e-2
