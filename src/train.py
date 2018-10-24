6c1f72cc9b38b3f5c9e02d0f1a5bddc3f3745d8e
Yuchen Li <ychnlgy@gmail.com>
2018-10-23 20:16:12 -0400

    Trying bottleneck

--- src/train.py
+++ src/train.py
@@ -200,6 +200,10 @@ class Model(models.Savable):
             torch.nn.LeakyReLU(),
             torch.nn.BatchNorm2d(64),
             
+            torch.nn.Conv2d(64, 64, 3, padding=1),
+            torch.nn.LeakyReLU(),
+            torch.nn.BatchNorm2d(64),
+            
             # 7 -> 14
             
             torch.nn.Upsample(scale_factor=2),
