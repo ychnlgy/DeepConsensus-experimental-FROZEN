191af84ace0eb477318c3b0ee0a51fe97e824c8e
Yuchen Li <ychnlgy@gmail.com>
2018-10-25 23:35:29 -0400

    Trying classifier on MNIST

--- src/models/Classifier.py
+++ src/models/Classifier.py
@@ -8,15 +8,14 @@ class Classifier(torch.nn.Module):
         super(Classifier, self).__init__()
         self.grp = torch.nn.Parameter(torch.rand(classes, hiddensize))
         self.cos = models.CosineSimilarity()
-        self.max = torch.nn.Softmax(dim=1)
     
     def get_class_vec(self, c):
         return self.grp[c]
     
     def forward(self, X):
-        cs = self.cos(X, self.grp)
-        confidence, indices = cs.max(dim=1)
-        confidence = confidence.view(-1, 1)
-        confusion = self.max(cs)
-        assert len(confidence) == len(confusion)
-        return confidence * confusion
+        return self.cos(X, self.grp)
+#        confidence, indices = cs.max(dim=1)
+#        confidence = confidence.view(-1, 1)
+#        confusion = self.max(cs)
+#        assert len(confidence) == len(confusion)
+#        return confidence * confusion
