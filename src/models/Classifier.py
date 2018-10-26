c9ce1dbf2ff8c63644fee28591198f09e0017500
Yuchen Li <ychnlgy@gmail.com>
2018-10-26 15:47:32 -0400

    Trying distillnet with softmax ahead of sum

--- src/models/Classifier.py
+++ src/models/Classifier.py
@@ -7,7 +7,8 @@ class Classifier(torch.nn.Module):
     def __init__(self, hiddensize, classes):
         super(Classifier, self).__init__()
         self.init_groups(classes, hiddensize)
-        self.diff = models.SoftminNorm()
+        self.diff = models.Norm()
+        self.min = torch.nn.Softmin(dim=1)
 #        self.cos = models.CosineSimilarity()
 #        self.max = torch.nn.Softmax(dim=1)
     
@@ -15,7 +16,7 @@ class Classifier(torch.nn.Module):
         self.grp = torch.nn.Parameter(torch.rand(classes, hiddensize))
     
     def forward(self, X):
-        return self.diff(X, self.grp)
+        return self.min(self.diff(X, self.grp))
     
 #    def get_class_vec(self, c):
 #        return self.grp[c]
