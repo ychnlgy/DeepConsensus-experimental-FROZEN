bfb6e0cf006a71b55bf7462be3bc2b86f38b78f9
Yuchen Li <ychnlgy@gmail.com>
2018-10-26 12:49:11 -0400

    Trying new separator model

--- src/models/Classifier.py
+++ src/models/Classifier.py
@@ -7,7 +7,7 @@ class Classifier(torch.nn.Module):
     def __init__(self, hiddensize, classes):
         super(Classifier, self).__init__()
         self.grp = torch.nn.Parameter(torch.rand(classes, hiddensize))
-        self.cos = models.CosineSimilarity(dim=1)
+        self.cos = models.CosineSimilarity()
         self.max = torch.nn.Softmax(dim=1)
     
     def get_class_vec(self, c):
