5786c546fcef6e8dd47c7c8bfded74c2f52e080e
Yuchen Li <ychnlgy@gmail.com>
2018-10-26 18:05:42 -0400

    Trying mnist64

--- src/models/Classifier.py
+++ src/models/Classifier.py
@@ -9,7 +9,6 @@ class Classifier(torch.nn.Module):
         self.init_groups(classes, hiddensize)
         self.cos = models.CosineSimilarity()
         self.max = torch.nn.Softmax(dim=1)
-        self.min = torch.nn.Softmin(dim=1)
     
     def init_groups(self, classes, hiddensize):
         self.grp = torch.nn.Parameter(torch.rand(classes, hiddensize))
@@ -18,13 +17,9 @@ class Classifier(torch.nn.Module):
         return self.grp[c]
     
     def forward(self, X):
-        N, D = X.size()
-        norm = (X.view(N, 1, D) - self.grp.view(1, -1, D)).abs().sum(dim=-1)
-        norm = self.min(norm)
-        
         cs = self.cos(X, self.grp)
         confidence, indices = cs.max(dim=1)
         confidence = confidence.view(-1, 1)
         confusion = self.max(cs)
         assert len(confidence) == len(confusion)
-        return confidence * confusion * norm
+        return confidence * confusion
