dd2d69cae35aec1f6de064a60791c97c339c7476
Yuchen Li <ychnlgy@gmail.com>
2018-10-26 21:53:47 -0400

    Trying confusion mechanism

--- src/models/DistillNet.py
+++ src/models/DistillNet.py
@@ -5,14 +5,28 @@ class DistillNet(torch.nn.Module):
     def __init__(self, *layers):
         super(DistillNet, self).__init__()
         self.layers = torch.nn.ModuleList(layers)
+        self.max = torch.nn.Softmax(dim=1)
     
     def forward(self, X):
-        vecs = sum(self.iter_forward(X))
+        vecs = sum(self.combine(self.iter_forward(X)))
         return vecs
         #return torch.cat(vecs, dim=1) # N, C1 + C2...
     
     def iter_forward(self, X):
         for layer in self.layers:
             X, vec = layer(X)
-            print(vec[0])
             yield vec
+    
+    def combine(self, it):
+        a = next(it)
+        b = next(it)
+        m = self.max(a)
+        n = self.max(b)
+        p = None
+        yield a * n
+        for c in it:
+            p = self.max(c)
+            yield m * b * p
+            a, b = b, c
+            m, n = n, p
+        yield m * b
