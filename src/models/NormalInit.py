e7b85d0946b70627c9dd26aacb90e047487b40d2
Yuchen Li <ychnlgy@gmail.com>
2018-10-31 15:07:58 -0400

    Trying xavier normal init

--- src/models/NormalInit.py
+++ src/models/NormalInit.py
@@ -3,11 +3,15 @@ import torch
 class NormalInit:
     
     def init_weights(self, sequential):
-        assert isinstance(sequential, torch.nn.Sequential)
+        t = type(sequential)
         target_types = self.get_init_targets()
-        for module in sequential:
-            if type(module) in target_types:
-                torch.nn.init.xavier_uniform(module.weight)
+        if t in target_types:
+            torch.nn.init.xavier_uniform(sequential.weight)
+        else:
+            assert isinstance(sequential, torch.nn.Sequential):
+            for module in sequential:
+                if type(module) in target_types:
+                    torch.nn.init.xavier_uniform(module.weight)
     
     def get_init_targets(self):
         raise NotImplementedError
