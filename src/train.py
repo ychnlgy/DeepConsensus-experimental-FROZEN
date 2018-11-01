afd151005dea9e7e4cfc7334f103bd88d86c5d40
Yuchen Li <ychnlgy@gmail.com>
2018-10-31 20:50:28 -0400

    Printing deepfool norm1

--- src/train.py
+++ src/train.py
@@ -84,7 +84,7 @@ def main(modelf, dataset, epochs, fool=0, classic=0, trainbatch=100, testbatch=3
             collect_answer(model, image)
             collect_answer(model, pert_image)
             
-            print("Pertubation norm-1: %.3f" % numpy.mean(r_tot**2))
+            print("Pertubation norm-1: %.3f" % numpy.sum(numpy.abs(r_tot)))
             
         raise SystemExit(0)
         
