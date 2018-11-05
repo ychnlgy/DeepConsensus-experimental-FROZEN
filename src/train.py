d112d8e632a0ddd7cab5d395db0bd3a82ee4f469
Yuchen Li <ychnlgy@gmail.com>
2018-11-05 13:19:56 -0500

    Using rbf nets

--- src/train.py
+++ src/train.py
@@ -202,9 +202,9 @@ def main(
             
             for i, X, y, bar in iter_dataloader(testloader, device, silent=True):
             
-                misc.debug.ALLOW_PRINTING = i < 5
-                misc.debug.println("")
-                misc.debug.println(y[0])
+#                misc.debug.ALLOW_PRINTING = i < 5
+#                misc.debug.println("")
+#                misc.debug.println(y[0])
                 X = get_fake(X)
             
                 yh = model(X)
