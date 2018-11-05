da23a33e799b511522f7c5da67a5e1c3f4d60b62
Yuchen Li <ychnlgy@gmail.com>
2018-11-05 13:20:33 -0500

    Using rbf nets

--- src/deepconsensus.py
+++ src/deepconsensus.py
@@ -61,58 +61,58 @@ class Model(ResNet):
                 ),
             ),
             
-            models.GlobalSumPool(
-                h = models.RbfNet(
-                    headsize = 128,
-                    bodysize = 64,
-                    tailsize = self.squash[5],
-                    layers = self.layers,
-                    dropout = 0.2,
-                    activation = self.act,
-                    bias = self.usebias
-                ),
-                c = models.Classifier(
-                    self.squash[5],
-                    classes + self.optout,
-                    useprototype = self.useprototype,
-                    usenorm = self.usenorm,
-                    p = self.p
-                ),
-                g = models.DenseNet(
-                    headsize = 128,
-                    bodysize = 64,
-                    tailsize = 1,
-                    layers = self.layers,
-                    dropout = 0.2,
-                    activation = torch.nn.ReLU()
-                ),
-            ),
-            
-            models.GlobalSumPool(
-                h = models.RbfNet(
-                    headsize = 256,
-                    bodysize = 64,
-                    tailsize = self.squash[7],
-                    layers = self.layers,
-                    dropout = 0.2,
-                    activation = self.act,
-                    bias = self.usebias
-                ),
-                c = models.Classifier(
-                    self.squash[7],
-                    classes + self.optout,
-                    useprototype = self.useprototype,
-                    usenorm = self.usenorm,
-                    p = self.p
-                ),
-                g = models.DenseNet(
-                    headsize = 256,
-                    bodysize = 64,
-                    tailsize = 1,
-                    layers = self.layers,
-                    dropout = 0.2,
-                    activation = torch.nn.ReLU()
-                ),
+#            models.GlobalSumPool(
+#                h = models.RbfNet(
+#                    headsize = 128,
+#                    bodysize = 64,
+#                    tailsize = self.squash[5],
+#                    layers = self.layers,
+#                    dropout = 0.2,
+#                    activation = self.act,
+#                    bias = self.usebias
+#                ),
+#                c = models.Classifier(
+#                    self.squash[5],
+#                    classes + self.optout,
+#                    useprototype = self.useprototype,
+#                    usenorm = self.usenorm,
+#                    p = self.p
+#                ),
+#                g = models.DenseNet(
+#                    headsize = 128,
+#                    bodysize = 64,
+#                    tailsize = 1,
+#                    layers = self.layers,
+#                    dropout = 0.2,
+#                    activation = torch.nn.ReLU()
+#                ),
+#            ),
+#            
+#            models.GlobalSumPool(
+#                h = models.RbfNet(
+#                    headsize = 256,
+#                    bodysize = 64,
+#                    tailsize = self.squash[7],
+#                    layers = self.layers,
+#                    dropout = 0.2,
+#                    activation = self.act,
+#                    bias = self.usebias
+#                ),
+#                c = models.Classifier(
+#                    self.squash[7],
+#                    classes + self.optout,
+#                    useprototype = self.useprototype,
+#                    usenorm = self.usenorm,
+#                    p = self.p
+#                ),
+#                g = models.DenseNet(
+#                    headsize = 256,
+#                    bodysize = 64,
+#                    tailsize = 1,
+#                    layers = self.layers,
+#                    dropout = 0.2,
+#                    activation = torch.nn.ReLU()
+#                ),
             )
         ]
     
