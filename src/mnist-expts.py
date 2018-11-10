484b8fea3feef7f11d4d43b7ef4db01acf91e9da
Yuchen Li <ychnlgy@gmail.com>
2018-11-09 20:34:33 -0500

    Added batch mnist tests

---
+++ src/mnist-expts.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import os
+
+import misc
+
+CMD = "python3 -W ignore ./train.py modelf=model.torch dataset=mnist64-corrupt epochs=30 modelid={modelid} min{t}={v} max{t}={v}"
+
+CMDS = []
+
+def do_tv(t, vmin, vmax, vstep):
+    for modelid in [2, 3]:
+        for v in range(vmin, vmax+vstep, vstep):
+            CMDS.append(CMD.format(modelid=modelid, t=t, v=v))
+
+@misc.main
+def main(gid):
+    if gid == "1":
+        do_tv("trans", 5, 20, 5)
+        do_tv("mag", 1.25, 2.0, 0.25)
+    elif gid == "2":
+        do_tv("rot", 15, 60, 15)
+        do_tv("gauss", 15, 60, 15)
+    elif gid == "3":
+        do_tv("sigma", 0.4, 1.6, 0.4)
+        do_tv("beta", 0.85, 0.4, -0.15)
+    else:
+        raise AssertionError
+    
+    for cmd in CMDS:
+        print(cmd)
+    
+    for cmd in CMDS:
+        os.system(cmd)
