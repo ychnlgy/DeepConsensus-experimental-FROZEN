7905514ebf727ce320c92fcba28bf1a9918d5b09
Yuchen Li <ychnlgy@gmail.com>
2018-11-10 22:28:07 -0500

    Added P4M convs

--- src/groupy_models/mnist/mnist.py
+++ src/groupy_models/mnist/mnist.py
@@ -59,7 +59,7 @@ class Net(nn.Module):
         self.conv2 = P4ConvP4(10, 10, kernel_size=3)
         self.conv3 = P4ConvP4(10, 20, kernel_size=3)
         self.conv4 = P4ConvP4(20, 20, kernel_size=3)
-        self.fc1 = nn.Linear(4*4*20*4, 50)
+        self.fc1 = nn.Linear(5*5*20*4, 50)
         self.fc2 = nn.Linear(50, 10)
 
     def forward(self, x):
@@ -69,8 +69,6 @@ class Net(nn.Module):
         x = F.relu(self.conv3(x))
         x = F.relu(self.conv4(x))
         x = plane_group_spatial_max_pooling(x, 2, 2)
-        print(x.shape)
-        input()
         x = x.view(x.size()[0], -1)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
