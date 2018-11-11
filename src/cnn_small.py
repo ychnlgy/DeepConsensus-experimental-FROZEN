3c0329e564651eda21f673f3c93bbab1314b4309
Yuchen Li <ychnlgy@gmail.com>
2018-11-11 14:32:56 -0500

    Added small CNN

--- src/cnn_small.py
+++ src/cnn_small.py
@@ -39,20 +39,20 @@ class Cnn(models.Savable):
                 torch.nn.LeakyReLU(),
 
                 # 32 -> 16
-                torch.nn.Conv2d(32, 64, 3, padding=1),
+                torch.nn.Conv2d(32, 32, 3, padding=1),
                 torch.nn.MaxPool2d(2),
-                torch.nn.BatchNorm2d(64),
+                torch.nn.BatchNorm2d(32),
                 torch.nn.LeakyReLU(),
                 
-                torch.nn.Conv2d(64, 64, 3, padding=1),
-                torch.nn.BatchNorm2d(64),
+                torch.nn.Conv2d(32, 32, 3, padding=1),
+                torch.nn.BatchNorm2d(32),
                 torch.nn.LeakyReLU(),
             ),
             
             
             torch.nn.Sequential(
                 # 16 -> 8
-                torch.nn.Conv2d(64, 64, 3, padding=1),
+                torch.nn.Conv2d(32, 64, 3, padding=1),
                 torch.nn.MaxPool2d(2),
                 torch.nn.BatchNorm2d(64),
                 torch.nn.LeakyReLU(),
