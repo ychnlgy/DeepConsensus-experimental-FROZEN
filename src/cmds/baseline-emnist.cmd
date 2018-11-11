297478a85ab1f9a34482722020b9b7858a37d0ae
Yuchen Li <ychnlgy@gmail.com>
2018-11-11 13:56:09 -0500

    Added baseline commands

--- src/cmds/baseline-emnist.cmd
+++ src/cmds/baseline-emnist.cmd
@@ -1,2 +1,2 @@
-./train.py epochs=30 repeat=10 dataset=emnist64-corrupt modelid=2 split=balanced
-./train.py epochs=30 repeat=10 dataset=emnist64-corrupt modelid=3 split=balanced
+./train.py epochs=30 repeat=10 dataset=emnist64-corrupt modelid=2 split=balanced modelf=model.torch
+./train.py epochs=30 repeat=10 dataset=emnist64-corrupt modelid=3 split=balanced modelf=model.torch
--- src/cmds/baseline-mnist.cmd
+++ src/cmds/baseline-mnist.cmd
@@ -1,2 +1,2 @@
-./train.py epochs=30 repeat=10 dataset=mnist64-corrupt modelid=2
-./train.py epochs=30 repeat=10 dataset=mnist64-corrupt modelid=3
+./train.py epochs=30 repeat=10 dataset=mnist64-corrupt modelid=2 modelf=model.torch
+./train.py epochs=30 repeat=10 dataset=mnist64-corrupt modelid=3 modelf=model.torch
